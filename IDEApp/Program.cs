3297ebc83a667e172db8c3a1bd39796769e5f4f5
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-09-16 15:02:38 +0530

    Add project files.

---
+++ IDEApp/Program.cs
@@ -0,0 +1,77 @@
+﻿class Program
+{
+    static void Main()
+    {
+        IDE ide = new IDE();
+        ide.Work();
+    }
+}
+
+
+class IDE // OCP
+{
+    LangC c = new LangC();
+    LangCSharp cs = new LangCSharp();
+    LangJava java = new LangJava();
+
+    public void Work() // DRY
+    {
+        Console.WriteLine(c.GetName());
+        Console.WriteLine(c.GetUnit());
+        Console.WriteLine(c.GetParadigm());
+        Console.WriteLine("-----------------");
+        Console.WriteLine(cs.GetName());
+        Console.WriteLine(cs.GetUnit());
+        Console.WriteLine(cs.GetParadigm());
+        Console.WriteLine("-----------------");
+        Console.WriteLine(java.GetName());
+        Console.WriteLine(java.GetUnit());
+        Console.WriteLine(java.GetParadigm());
+
+    }
+}
+class LangCSharp
+{
+    public string GetName()
+    {
+        return "C# Language";
+    }
+    public string GetUnit()
+    {
+        return "Class";
+    }
+    public string GetParadigm()
+    {
+        return "Object Oriented";
+    }
+}
+class LangJava
+{
+    public string GetName()
+    {
+        return "Java Language";
+    }
+    public string GetUnit()
+    {
+        return "Class";
+    }
+    public string GetParadigm()
+    {
+        return "Object Oriented";
+    }
+}
+class LangC
+{
+    public string GetName()
+    {
+        return "C Language";
+    }
+    public string GetUnit()
+    {
+        return "Function";
+    }
+    public string GetParadigm()
+    {
+        return "Procedural Oriented";
+    }
+}
