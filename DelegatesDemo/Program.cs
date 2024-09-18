1db1823bd8b834c5df8e043ed9954cf2386faca3
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-09-18 11:08:01 +0530

    delegates

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace DelegatesDemo
+{
+
+    //class MyDelegate : Delegate
+    //{
+
+    //}
+    //1. Declare the delegate
+    delegate void MyDelegate(string str);
+    delegate int MyDelegate2(string str);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. direct - invoke by name
+
+            //A. Name of the method
+            //B. Singnature of the method
+            //C. type of the method
+            //c1. access specifier
+            //D. Which class?
+            //E. Which namespace
+            //F. Which assembly
+
+
+
+            //Greeting("calling directly");
+
+
+
+            //2. indirect - invoke by addresss
+            // delegate
+            // sign + address
+
+            //1. Declare the delegate
+            //Delegate d = new Delegate();
+
+            //2. Instantiation
+            MyDelegate d = new MyDelegate(Program.Greeting);
+            Program p = new Program();
+            d += p.Hello; // subscription
+            d -= Greeting; // Unsubscription
+            d += Greeting;
+            //3. Initialization
+            //5. Invoke
+            //d.Invoke("calling indirectly from delegate");
+            d("calling indirectly from delegate");
+
+        }
+
+
+
+        static void Greeting(string msg)
+        {
+            Console.WriteLine($"Greeting: {msg}");
+        }
+
+        void Hello(string msg)
+        {
+            Console.WriteLine($"Hello: {msg}");
+            return 0;
+        }
+    }
+}
---
+++ DelegatesDemo2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace DelegatesDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // client 1 - list all running proc names
+            //ProcessManager.ShowProcessList();
+
+            // client 2 - display only proc sw S
+            //ProcessManager.ShowProcessList("S");
+
+            //client 3 - display proce mem size > 50MB
+            ProcessManager.ShowProcessList(500 * 1024 * 1024);
+        }
+    }
+
+
+
+    // backend dev
+    class ProcessManager
+    {
+        public static void ShowProcessList()
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                Console.WriteLine(p.ProcessName);
+            }
+        }
+        public static void ShowProcessList(string sw)
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.ProcessName.StartsWith(sw))
+                    Console.WriteLine(p.ProcessName);
+            }
+        }
+
+        public static void ShowProcessList(long size)
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.WorkingSet64 >= size)
+                    Console.WriteLine(p.ProcessName);
+            }
+        }
+    }
+}
