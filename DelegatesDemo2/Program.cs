2e1d53830a0d95b6387001ae66bc08304613dc9a
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2024-09-18 12:42:38 +0530

    delegates demo

--- DelegatesDemo2/Program.cs
+++ DelegatesDemo2/Program.cs
@@ -7,32 +7,59 @@ namespace DelegatesDemo2
         static void Main(string[] args)
         {
             // client 1 - list all running proc names
-            ProcessManager.ShowProcessList(NoFilter);
+            //ProcessManager.ShowProcessList(NoFilter);
+
+
+            // Anonymous Delegates
+            //ProcessManager.ShowProcessList(delegate { return true; });
+
 
             // client 2 - display only proc sw S
-            //ProcessManager.ShowProcessList(FilterByName);
+            ProcessManager.ShowProcessList(delegate (Process p)
+            {
+                return p.ProcessName.StartsWith("S");
+            });
+
+            // Lambda - statements/expressions : light weight syntax for anonymous delegates
+
+
+            // Lambda statement
+
+            ProcessManager.ShowProcessList((Process p) =>
+            {
+                return p.ProcessName.StartsWith("S");
+            });
+
+            // Lambda expression 1
+
+            ProcessManager.ShowProcessList((Process p) =>
+            p.ProcessName.StartsWith("S"));
+
+            // Lambda expression 2
+            ProcessManager.ShowProcessList(x => x.ProcessName.StartsWith("S"));
 
             //client 3 - display proce mem size > 50MB
             //FilterDelegate filter = new FilterDelegate(FilterByMemSize);
             //ProcessManager.ShowProcessList(filter);
+            ProcessManager.ShowProcessList(p => p.WorkingSet64 >= 500 * 1024 * 1024);
         }
         // client 1
 
-        static bool NoFilter(Process process) { return true; }
+        //static bool NoFilter(Process process) { return true; }
 
 
         //client 2
-        static bool FilterByName(Process p)
-        {
-            return p.ProcessName.StartsWith("S");
-        }
+        //static bool FilterByName(Process p)
+        //{
+        //    return p.ProcessName.StartsWith("S");
+        //}
 
 
         // client 3
-        static bool FilterByMemSize(Process p)
-        {
-            return p.WorkingSet64 >= 500 * 1024 * 1024;
-        }
+        //public static bool FilterByMemSize(Process p)
+        //{
+        //    return p.WorkingSet64 >= 500 * 1024 * 1024;
+        //}
     }
 
 
@@ -47,6 +74,7 @@ namespace DelegatesDemo2
             foreach (Process p in Process.GetProcesses())
             {
                 if (filter(p))
+                    //if(Program.FilterByMemSize(p))
                     Console.WriteLine(p.ProcessName);
             }
         }
---
+++ DeletesDemo3/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace DeletesDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+            // find evens sum
+
+            int evensSum = 0;
+
+            foreach (int i in list)
+            {
+                if (i % 2 == 0)
+                {
+                    evensSum += i;
+                }
+            }
+
+            // fw
+            Func<int, bool> filter = new Func<int, bool>(IsEven);
+            evensSum = list.Where(filter).Sum();
+
+            evensSum = list.Where(IsEven).Sum();
+
+            // Anonymous
+
+            evensSum = list.Where(delegate (int x)
+            {
+                return x % 2 == 0;
+            }).Sum();
+
+            // Lambda statement
+
+            evensSum = list.Where((int x) =>
+            {
+                return x % 2 == 0;
+            }).Sum();
+
+            // Lambda expression
+
+            evensSum = list.Where(x => x % 2 == 0).Sum();
+        }
+
+        static bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+    }
+}
